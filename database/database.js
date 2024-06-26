64a3ab0d3c2d6e3ef7e6e7c9709bc270b70d094f
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-06-26 20:08:45 +0530

    database updated with the attendees table

--- database/database.js
+++ database/database.js
@@ -5,6 +5,9 @@ db.serialize(() => {
   db.run(
     "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, venue TEXT, date DATE DEFAULT (date('now')))"
   );
+  db.run(
+    "CREATE TABLE IF NOT EXISTS attendees (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, contact TEXT, event_id INTEGER, FOREIGN KEY(event_id) REFERENCES events(id))"
+  );
 });
 
 module.exports = db;
