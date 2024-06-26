2d6902d1fede429e57106e336642c38c6e163501
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-06-26 20:56:47 +0530

    new api start interface

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const ParseResponse = require("./utils/response");
 const express = require("express");
 require("dotenv").config();
+const figlet = require("figlet");
+const logError = require("./utils/logger");
 
 //var declarations
 const app = express();
@@ -16,6 +18,21 @@ app.get("/api/", (req, res) => {
 const eventRoutes = require("./routes/event");
 app.use("/api/v1/events", eventRoutes);
 
-app.listen(port, () => {
-  console.log(`Service started on ${port} at ${new Date()}`);
+app.listen(port, async () => {
+  const chalk = await import("chalk");
+  figlet("E.M. API", (err, data) => {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      logError(err);
+      return;
+    }
+
+    console.log(chalk.default.green(data));
+    console.log(chalk.default.blue(`Service started on port ${port}`));
+    console.log(
+      chalk.default.yellow(`Startup time: ${new Date().toLocaleString()}`)
+    );
+    console.log(chalk.default.green("Ready to handle requests..."));
+  });
 });
