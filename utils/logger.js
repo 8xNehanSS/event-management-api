40002bbcfc254ee65933592d50a85cec3af50a84
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-06-26 21:01:21 +0530

    error log typo fix in path

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 function logError(error) {
   const logMessage = `[${new Date().toISOString()}] ERROR: ${error}\n`;
 
-  fs.appendFile("../logs/error.log", logMessage, (err) => {
+  fs.appendFile("logs/error.log", logMessage, (err) => {
     if (err) {
       console.error("Failed to write to error.log:", err);
     }
