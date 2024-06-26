1b295c6f0e0934fe50349896b3c18582c0581ebf
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-06-26 20:13:50 +0530

    attendee add route added

--- routes/event.js
+++ routes/event.js
@@ -7,5 +7,6 @@ router.get("/", eventController.getEvents);
 router.get("/:id", eventController.getEvent);
 router.put("/:id", eventController.updateEvent);
 router.delete("/:id", eventController.deleteEvent);
+router.post("/:id/attendee", eventController.addAttendee);
 
 module.exports = router;
