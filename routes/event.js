a68d46378d7580c932d193a2dd9233b36ee33d22
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-06-26 20:34:50 +0530

    updated routes with new names

--- routes/event.js
+++ routes/event.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
 
-router.post("/", eventController.addEvent);
-router.get("/", eventController.getEvents);
-router.get("/:id", eventController.getEvent);
-router.put("/:id", eventController.updateEvent);
-router.delete("/:id", eventController.deleteEvent);
-router.post("/:id/attendee", eventController.addAttendee);
-router.delete("/:id/attendee", eventController.removeAttendee);
-router.get("/:id/attendees", eventController.getEventAttendees);
+router.post("/add-event", eventController.addEvent);
+router.get("/get-all-events", eventController.getEvents);
+router.get("/get-event/:id", eventController.getEvent);
+router.put("/update-event/:id", eventController.updateEvent);
+router.delete("/delete-event/:id", eventController.deleteEvent);
+router.post("/add-attendee/:id", eventController.addAttendee);
+router.delete("/remove-attendee/:id", eventController.removeAttendee);
+router.get("/get-attendees/:id", eventController.getEventAttendees);
 
 module.exports = router;
